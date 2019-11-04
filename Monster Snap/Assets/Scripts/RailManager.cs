bea439975678c7679e561c98d783b38b0d6f8d6b
Philip Park <philippark0203@gmail.com>
2019-11-04 01:43:32 -0500

    Made initial Unity project! Added basic GameObjects and rail movement.

---
+++ Monster Snap/Assets/Scripts/RailManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailManager : MonoBehaviour
+{
+
+    public Transform location1;
+    public Transform location2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<RailMovement>().location = location1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Location1")
+        {
+            GetComponent<RailMovement>().location = location2;
+        }
+    }
+}
---
+++ Monster Snap/Assets/Scripts/RailMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailMovement : MonoBehaviour
+{
+    public float speed;
+    public Transform location;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, location.position, speed * Time.deltaTime);
+    }
+}
