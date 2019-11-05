e622315ee5b517ad093bc53006d781893063379b
Alekai McAdam <am8578@nyu.edu>
2019-11-05 11:46:01 -0500

    - press z to switch cameras (temp fake zoom) - started mouse follow script but it doesn't work properly yet

---
+++ Monster Snap/Assets/Scripts/camFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camFollow : MonoBehaviour
+{
+    private GameObject mainCam;
+    private GameObject newCam;
+
+    public float followSpeed;
+    
+    void Start()
+    {
+        newCam = GameObject.Find("Camera");
+        mainCam = GameObject.Find("Main Camera");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // follow mouse (doesn't work properly rn)
+        
+        /* if (newCam.activeSelf)
+        {
+            newCam.transform.LookAt(newCam.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                Input.mousePosition.y, newCam.GetComponent<Camera>().nearClipPlane)), Vector3.up);
+        }
+        else
+        {
+            mainCam.transform.LookAt(mainCam.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                Input.mousePosition.y, mainCam.GetComponent<Camera>().nearClipPlane)), Vector3.up);
+        } */
+    }
+}
---
+++ Monster Snap/Assets/Scripts/camZoom.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camZoom : MonoBehaviour
+{
+    private GameObject mainCam;
+    private GameObject newCam;
+
+    public KeyCode zoomKey;
+    public float zoomDist;
+    
+    void Start()
+    {
+        newCam = GameObject.Find("Camera");
+        mainCam = GameObject.Find("Main Camera");
+        
+        setMainCamActive();
+    }
+    
+    void Update()
+    {
+        // cam switch
+        if (Input.GetKeyDown(zoomKey))
+        {
+            setNewCamActive();
+        } else if (Input.GetKeyUp(zoomKey))
+        {
+            setMainCamActive();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // zoom ??
+    }
+
+    void setMainCamActive()
+    {
+        mainCam.SetActive(true);
+        newCam.SetActive(false);
+    }
+
+    void setNewCamActive()
+    {
+        mainCam.SetActive(false);
+        newCam.SetActive(true);
+    }
+}
