880a71cf553d266f0476eee4816165360f34d692
Geneva Heyward <geneva.heyward@urbanarts.org>
2019-12-03 15:27:42 -0500

    Added in UI art and placed it in the proper scenes. Made it so in the creature name scene it displays their tags ( fish, sleepy boi, etc..)

---
+++ Monster Snap/Assets/NamePrompter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NamePrompter : MonoBehaviour // works with creature name controller to prompt the player to name each creature
+{
+    CreatureNameController cnc;
+    TextMeshProUGUI _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        cnc = FindObjectOfType<CreatureNameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _text.text = "Give this " + cnc.monstersToBeNamed[cnc.posInList] + " a name!";   
+    }
+}
